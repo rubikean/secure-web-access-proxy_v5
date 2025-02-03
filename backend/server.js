1a839697e17ddf416d883ee5a35dfc6438d62728
unknown <ckean435@gmail.com>
2025-02-03 09:49:26 -0500

    update to V6

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+{
+  "name": "secure-web-access-proxy_v5",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "bcryptjs": "^2.4.3",
+    "connect-pg-simple": "^10.0.0",
+    "dotenv": "^16.4.7",
+    "express": "^4.21.2",
+    "express-session": "^1.18.1",
+    "jsonwebtoken": "^9.0.2",
+    "pg": "^8.13.1"
+  }
+}
